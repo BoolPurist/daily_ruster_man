31cb697e7c82fe8eb558de563dbeefc068729d2e
BoolPurist <fnaumann@posteo.de>
2023-03-19 16:52:05 +0100

    refactor: dependency injection for executing editor on journal

--- src/core/process_handling.rs
+++ src/core/process_handling.rs
@@ -5,6 +5,42 @@ use std::process::Command;
 
 use super::app_options::AppOptions;
 
+pub trait ProcessExecuter {
+    fn start_program(&self, option: &AppOptions, editor: &str, path: &Path) -> AppResult;
+}
+
+#[derive(Default)]
+pub struct RealProcessExecuter {}
+
+impl ProcessExecuter for RealProcessExecuter {
+    fn start_program(&self, option: &AppOptions, editor: &str, path: &Path) -> AppResult {
+        let path_as_str = path
+            .to_str()
+            .ok_or_else(|| anyhow!("Could not convert path to a text as argument for editor."))?;
+
+        debug!("Starting program {} with argument: {}", editor, path_as_str);
+
+        if !option.run_editor_dry() {
+            Command::new(editor)
+                .arg(path_as_str)
+                .spawn()
+                .map_err(AppError::new)
+                .with_context(|| {
+                    format!(
+                        "Failded to start editor {0} with args {path_as_str}
+Does {0} as an editor exits and is findable via $PATH ?",
+                        editor
+                    )
+                })?
+                .wait()
+                .map_err(AppError::new)
+                .context("Editor did run correctly")?;
+        }
+
+        Ok(())
+    }
+}
+
 pub fn start_process_with(option: &AppOptions, editor: &str, path: &Path) -> AppResult {
     let path_as_str = path
         .to_str()
