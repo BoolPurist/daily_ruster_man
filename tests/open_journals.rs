068947fdf3aee2fd7afac3e8448d2e53bd26fbbe
BoolPurist <fnaumann@posteo.de>
2023-03-20 16:36:16 +0100

    tests: test for open monthly/yearly journal and with values from configuration for journals

--- tests/open_journals.rs
+++ tests/open_journals.rs
@@ -1,11 +1,16 @@
 mod common;
-use std::path::PathBuf;
+use std::{path::PathBuf, fs};
 
 use common::FileTmpBuilder;
 use daily_ruster_man::{
     core::{
-        open_actions, process_handling::TestProcessExecuter,
-        date_models::units_validated::ValidatedDate, app_options::AppOptions,
+        open_actions,
+        process_handling::TestProcessExecuter,
+        date_models::{
+            units_validated::{ValidatedDate, ValidatedYear},
+            open_by::OpenByMonthInYear,
+        },
+        app_options::AppOptions,
     },
     cli::{app_args::GenerellArgs, edit_argument::EditCommonArgs},
 };
@@ -72,6 +77,113 @@ fn should_open_specific_daily_journal_with_specific_editor() {
     );
 }
 
+#[test]
+fn should_open_specific_monthly_journal() {
+    let processor = TestProcessExecuter::default();
+    let files = FileTmpBuilder::default().build();
+
+    let month_year = OpenByMonthInYear::WithYear {
+        month: 4.try_into().unwrap(),
+        year: 3555.try_into().unwrap(),
+    };
+
+    let app_options = AppOptions::with(GenerellArgs::new(
+        false,
+        None,
+        Some(files.path().to_str().unwrap().to_string()),
+    ));
+    let edit_option = EditCommonArgs::default();
+
+    let actual =
+        open_actions::open_by_month_year(&processor, month_year, &app_options, &edit_option);
+
+    assert_open_action(
+        processor,
+        files,
+        matches!(actual, Ok(None)),
+        "3555_04_monthly.md".into(),
+        "vim",
+    );
+}
+
+#[test]
+fn should_open_specific_yearly_journal() {
+    let processor = TestProcessExecuter::default();
+    let files = FileTmpBuilder::default().build();
+
+    let year: ValidatedYear = 1620.try_into().unwrap();
+
+    let app_options = AppOptions::with(GenerellArgs::new(
+        false,
+        None,
+        Some(files.path().to_str().unwrap().to_string()),
+    ));
+    let edit_option = EditCommonArgs::default();
+
+    let actual = open_actions::open_by_year(&processor, year, &app_options, &edit_option);
+
+    assert_open_action(
+        processor,
+        files,
+        matches!(actual, Ok(None)),
+        "1620_yearly.md".into(),
+        "vim",
+    );
+}
+
+#[test]
+fn should_open_journal_with_editor_and_init_template_from_conf() {
+    let processor = TestProcessExecuter::default();
+    const CONF_FILE_CONTENT: &str = r#"
+      editor = "nvim"
+      daily_template="+daily.template"
+        "#;
+    const FILE_TO_OPEN_PATH: &str = "2023_11_10_daily.md";
+    const TEMPLATE_FILE_CONTENT: &str = r#"
+        Some template
+        "#;
+
+    let files = FileTmpBuilder::default()
+        .with_file(
+            PathBuf::from("config.toml"),
+            Some(CONF_FILE_CONTENT.to_owned()),
+        )
+        .with_file(
+            PathBuf::from("daily.template"),
+            Some(TEMPLATE_FILE_CONTENT.to_owned()),
+        )
+        .build();
+
+    let date = ValidatedDate::new(
+        2023.try_into().unwrap(),
+        11.try_into().unwrap(),
+        10.try_into().unwrap(),
+    )
+    .expect("Invalid date");
+
+    let path = files.path().to_str().unwrap().to_string();
+    let app_options = AppOptions::with(GenerellArgs::new(false, Some(path.clone()), Some(path)));
+    let edit_option = EditCommonArgs::default();
+
+    let actual = open_actions::open_by_date(&processor, date, &app_options, &edit_option);
+
+    let open_file_path = files.path().join(FILE_TO_OPEN_PATH);
+    let file_content = fs::read_to_string(open_file_path)
+        .expect("Failed to read file which shoud have been opened with certain template.");
+
+    assert_eq!(
+        TEMPLATE_FILE_CONTENT, file_content,
+        "Opened file has not the template content"
+    );
+
+    assert_open_action(
+        processor,
+        files,
+        matches!(actual, Ok(None)),
+        FILE_TO_OPEN_PATH.into(),
+        "nvim",
+    );
+}
 #[test]
 fn should_open_specific_daily_journal_with_show_only() {
     let expected_content = Some("Some content".to_owned());
